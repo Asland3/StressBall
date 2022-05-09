e96c704d4d1079c3f3dba2c8d2da34e9a52414f6
Asland3 <malthe3@outlook.com>
2022-05-09 09:25:57 +0200

    test 3

---
+++ UDPBrodcaster/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using StressBall;
+
+Random randomSpeed = new Random();
+int nextTest = 1;
+Console.WriteLine("UDP Client");
+
+using (UdpClient socket = new UdpClient())
+{
+    while (true)
+    {
+        string sensorName = "UDPTest" + nextTest++;
+
+        StressBallData stressBallData = new StressBallData()
+        {
+            Id = 100,
+            Speed = (randomSpeed.Next(30, 150)).ToString(),
+            DateTimeNow = new DateTime(2022, 10, 3)
+        };
+        string message = JsonSerializer.Serialize(stressBallData);
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        Console.WriteLine("Broadcaster sent " + message);
+
+        socket.Send(data, data.Length, "127.0.0.1", 10100);
+        Thread.Sleep(3000);
+    }
+}
---
+++ UDPServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+
+Console.WriteLine("UDP Server");
+
+using (UdpClient socket = new UdpClient())
+using (HttpClient client = new HttpClient())
+{
+    {
+        IPEndPoint clientEndPoint = null;
+        socket.Client.Bind(new IPEndPoint(IPAddress.Any, 10100));
+        //
+        while (true)
+        {
+            byte[] data = socket.Receive(ref clientEndPoint);
+
+            string message = Encoding.UTF8.GetString(data);
+
+            Console.WriteLine("Server received: " + message);
+
+
+            HttpContent content = new StringContent(message, Encoding.UTF8, "application/json");
+            client.PostAsync("https://stressballapi.azurewebsites.net/api/StressBall", content);
+
+            socket.Send(data, data.Length, clientEndPoint); 
+        }
+    }
+}
